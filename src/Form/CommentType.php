62c12736b15ae1f5d2d6918347caa6a0f2acaeae
Maxime <maximeghislainyt@gmail.com>
2020-05-28 17:32:15 +0200

    Responsive design home,ranking,teams,stades

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -7,6 +7,7 @@ use App\Form\ApplicationType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -15,13 +16,7 @@ class CommentType extends ApplicationType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            /*->add('rating', IntegerType::class, $this->getConfiguration("Avis","Indiquez votre note de 0 à 5",[
-                'attr' => [
-                    'min' => 0,
-                    'max' => 5,
-                    'step' => 1
-                ]
-            ]))*/
+            ->add('rating', HiddenType::class, $this->getConfiguration(false,false))
             ->add('content', TextareaType::class, [
                 'label' => false,
                 'attr' => [
